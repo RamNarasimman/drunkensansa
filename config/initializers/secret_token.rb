d71a98947ff6cef64733949c91edeb492d3f0c55
r4mb0 <ram@r4mbo.com>
2013-03-26 17:17:54 +1100

    firstscaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Firstscaffold::Application.config.secret_token = '77ab877edf870100963731c773cc5ebfc21050a5a048c1d2cd9e78ef299f90e922fe639a4e0024f7acefe21b75bb6fa9d1dd1614597c9b2b13d9cba4f67ff728'
